6421822ef061c44a1dbb246055e0e5fb9ed52957
adrianlucadan <adrian.luca.dan@gmail.com>
2019-05-05 23:56:27 +0300

    filter/delete books

--- assignment2/src/main/java/com/assignment2/demo/controller/BookController.java
+++ assignment2/src/main/java/com/assignment2/demo/controller/BookController.java
@@ -4,10 +4,8 @@ import com.assignment2.demo.model.Book;
 import com.assignment2.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,31 +14,58 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping("/addBook")
-    public ModelAndView addBook(Book book){
+    public ModelAndView addBook(Book book) {
         ModelAndView modelAndView = new ModelAndView("insertBookPage");
         bookService.addBook(book);
         return modelAndView;
     }
 
     @GetMapping("/addBook")
-    public ModelAndView displayInsertBookPage(){
+    public ModelAndView displayInsertBookPage() {
         ModelAndView modelAndView = new ModelAndView("insertBookPage");
-        modelAndView.addObject("book",new Book());
+        modelAndView.addObject("book", new Book());
         return modelAndView;
     }
 
     @GetMapping("/books")
-    public ModelAndView displayAllBooks(){
+    public ModelAndView displayAllBooks() {
         ModelAndView modelAndView = new ModelAndView("seeAllBooks");
-        modelAndView.addObject("books",bookService.seeAllBooks());
+        modelAndView.addObject("books", bookService.seeAllBooks());
         return modelAndView;
     }
 
+    @GetMapping("/books/author")
+    public ModelAndView selectBookByAuthor(@RequestParam(value = "author") String author) {
+        ModelAndView modelAndView = new ModelAndView("seeBooksByAuthor");
+        modelAndView.addObject("booksByAuthor", bookService.seeBooksByAuthor(author));
+        return modelAndView;
+    }
+
+    @GetMapping("/books/title")
+    public ModelAndView selectBookByTitle(@RequestParam(value = "title") String title) {
+        ModelAndView modelAndView = new ModelAndView("seeBooksByTitle");
+        modelAndView.addObject("booksByTitle", bookService.seeBooksByTitle(title));
+        return modelAndView;
+    }
+
+    @GetMapping("/books/genre")
+    public ModelAndView selectBooksByGenre(@RequestParam(value="genre") String genre){
+        ModelAndView modelAndView = new ModelAndView("seeBooksByGenre");
+        modelAndView.addObject("booksByGenre",bookService.seeBooksByGenre(genre));
+        return modelAndView;
+    }
+
+    @GetMapping("/books/year")
+    public ModelAndView selectBooksByYear(@RequestParam(value = "year") int year){
+        ModelAndView modelAndView = new ModelAndView("seeBooksByYear");
+        modelAndView.addObject("booksByYear", bookService.seeBooksByRelease(year));
+        return modelAndView;
+    }
 
     @DeleteMapping("books/{id}")
-    public ModelAndView deleteBook(@PathVariable("id") long id){
-        ModelAndView modelAndView = new ModelAndView("seeAllBooks");
-        bookService.deleteBook((int)id);
+    public ModelAndView deleteBook(@PathVariable("id") long id) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/books");
+        bookService.deleteBook((int) id);
         return modelAndView;
     }
 
